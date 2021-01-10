a05d14f7433728e726600c218251e9f087d74a16
priyanshu chauhan <priyanshuc.info@gmail.com>
2021-01-10 15:11:51 +0530

    Added few more

--- graphql-react-express/server/app.js
+++ graphql-react-express/server/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -11,17 +11,20 @@ app.use(cors());
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
-mongoose.connection.once('open', () => {
-    console.log('conneted to database');
-});
+// mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+// mongoose.connection.once('open', () => {
+//     console.log('conneted to database');
+// });
 
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+    console.log("now listening for requests on port 4000");
 });
